e0d2c4c028245afdd6d6b63a169a53dde766543e
Quang Dat <quangdat2000.pham@gmail.com>
2020-10-27 13:49:20 +0700

    Add baseUrl environment

--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -3,9 +3,11 @@ import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
+import { environment } from '../../../environments/environment';
+
 @Injectable()
 export class DataService {
-  private _baseUrl: string = 'http://localhost:8080';
+  private _baseUrl: string = environment.baseUrl;
 
   constructor(
     private http: HttpClient
