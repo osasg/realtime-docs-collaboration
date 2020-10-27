a7b0ea4a7ccf185b6561f4a96971520736c76d13
Quang Dat <quangdat2000.pham@gmail.com>
2020-10-27 14:50:47 +0700

    Add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './shared/';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialRootModule } from './material.module';
+
 import { AppComponent } from './app.component';
 import { AboutPageComponent } from './containers/AboutPage/about-page.component';
 import { AuthPageComponent } from './containers/AuthPage/auth-page.component';
 import { HomePageComponent } from './containers/HomePage/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,9 @@ import { HomePageComponent } from './containers/HomePage/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    MaterialRootModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
